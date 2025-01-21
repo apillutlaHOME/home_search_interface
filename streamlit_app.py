bf6f12dd351b0f593705d9d019100492816c14cb
apillutlaHOME <apoorva@imcominghome.io>
2025-01-21 11:36:17 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,1 @@
-import streamlit as st
-import streamlit.components.v1 as components
 
-st.title("Jupyterlite in Streamlit")
-st.sidebar.header("Configuration")
-components.iframe(
-    "https://jupyterlite.github.io/demo/repl/index.html?kernel=python&toolbar=1",
-    height=500
-)
